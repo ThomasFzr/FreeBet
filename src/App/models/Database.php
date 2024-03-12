bd1e85c69fb5d9bfa82ff02bd47a421cabc7b307
godafflig <tombenat99@hotmail.fr>
2024-03-12 14:36:10 +0100

    confilt fix

--- src/App/models/Database.php
+++ src/App/models/Database.php
@@ -30,15 +30,9 @@ class Database
         $sql = "SHOW TABLES LIKE 'User'";
         $result = $this->conn->query($sql);
         if ($result->rowCount() == 0) {
-<<<<<<< HEAD
-            $sql = "CREATE TABLE User (user_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255), surname VARCHAR(255), mail VARCHAR(255) NOT NULL, pwd VARCHAR(255) NOT NULL, phoneNbr VARCHAR(255), isAdmin BOOL NOT NULL DEFAULT false, coin INT, UNIQUE(mail));
-                        CREATE TABLE IF NOT EXISTS Football_match (match_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, date VARCHAR(255) NOT NULL, status VARCHAR(50) NOT NULL, opponent_team_id INT NOT NULL, opponent_team_name VARCHAR(255) NOT NULL, OL_score INT, opponent_score INT, victorious_team_id INT);
-                        CREATE TABLE IF NOT EXISTS Bet (bet_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, match_id INT NOT NULL, user_id INT NOT NULL, victorious_team_id INT NOT NULL, coin INT, updated BOOL default 0,FOREIGN KEY (match_id) REFERENCES Football_match(match_id), FOREIGN KEY (user_id) REFERENCES User(user_id));
-=======
                 $sql = "CREATE TABLE User (user_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255), surname VARCHAR(255), mail VARCHAR(255) NOT NULL, pwd VARCHAR(255) NOT NULL, phoneNbr VARCHAR(255), isAdmin BOOL NOT NULL DEFAULT false, coin INT, UNIQUE(mail));
                         CREATE TABLE IF NOT EXISTS Football_match (match_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, date VARCHAR(255) NOT NULL, status VARCHAR(50) NOT NULL, opponent_team_id INT NOT NULL, opponent_team_name VARCHAR(255) NOT NULL, OL_score INT, opponent_score INT);
                         CREATE TABLE IF NOT EXISTS Bet (match_id INT NOT NULL, user_id INT NOT NULL, victorious_team_id INT NOT NULL, coin INT, FOREIGN KEY (match_id) REFERENCES Football_match(match_id), FOREIGN KEY (user_id) REFERENCES User(user_id));
->>>>>>> 5c091c0 (add name of opponent team in bdd)
                         ";
             $this->conn->exec($sql);
             return true;
