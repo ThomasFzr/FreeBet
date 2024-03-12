c132be52943875d07f7d25d53a8a521359dffb8a
fthomas <thomas.foltzer@ynov.com>
2024-03-12 14:19:02 +0100

    add call api and test bet when login and home page

--- src/App/controllers/AddCoinController.php
+++ src/App/controllers/AddCoinController.php
@@ -3,8 +3,10 @@ class AddCoinController
 {
     public function AddCoin()
     {
-        require_once __DIR__ . '/../models/Bet.php';
-        $bet = new Bet();
-        $bet->UpdateCoinFromBet($_SESSION["userID"]);
+        if (isset($_SESSION['userID'])) {
+            require_once __DIR__ . '/../models/Bet.php';
+            $bet = new Bet();
+            $bet->UpdateCoinFromBet($_SESSION["userID"]);
+        }
     }
 }
